403913fcfaaba1ae1fc534598e7eeebed7e1953f
Lequocdat <54608114+lqdat@users.noreply.github.com>
2024-06-05 04:00:14 +0000

    add dockerfile

--- quack.js
+++ quack.js
@@ -1,5 +1,5 @@
 const loadToken = require("./modules/loadToken");
-const ACCESS_TOKEN =  process.env.QUACK_TOKEN ||   loadToken();
+const ACCESS_TOKEN = process.env.QUACK_TOKEN || loadToken();
 const randomUseragent = require("random-useragent");
 
 const ua = randomUseragent.getRandom((ua) => {
@@ -136,10 +136,12 @@ async function harvestAllEgg() {
 }
 
 harvestAllEgg();
-setTimeout(() => {
-  console.log('Hết thời gian 50p');
-  process.exit();
-}, 1200000);
+if (process.env.NONSTOP === 'true') {
+  setTimeout(() => {
+    console.log('Hết thời gian 50p');
+    process.exit();
+  }, 1200000);
+}
 
 
-module.exports=harvestAllEgg;
+module.exports = harvestAllEgg;
